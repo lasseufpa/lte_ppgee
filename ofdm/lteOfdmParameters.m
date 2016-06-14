1a949bb78032e326f67b0fe8efa7231751b85785
Igor Freire <igor.auad@gmail.com>
2016-06-14 19:03:45 -0300

    Minor improve of comments regarding LTE parameters

--- ofdm/lteOfdmParameters.m
+++ ofdm/lteOfdmParameters.m
@@ -5,31 +5,37 @@ function [ params ] = lteOfdmParameters( bw_choice )
 
 switch (bw_choice)
     case 1
+        % LTE 1.4 MHz
         nRBs = 6;
         W    = 1.4e6;
         fs   = 1.92e6;
         N    = 128;
     case 2
+        % LTE 3 MHz
         nRBs = 15;
         W    = 3e6;
         fs   = 3.84e6;
         N    = 256;
     case 3
+        % LTE 5 MHz
         nRBs = 25;
         W    = 5e6;
         fs   = 7.68e6;
         N    = 512;
     case 4
+        % LTE 10 MHz
         nRBs = 50;
         W    = 10e6;
         fs   = 15.36e6;
         N    = 1024;
     case 5
+        % LTE 15 MHz
         nRBs = 75;
         W    = 15e6;
         fs   = 23.04e6;
         N    = 1536;
     case 6
+        % LTE 20 MHz
         nRBs = 100;
         W    = 20e6;
         fs   = 30.72e6;
